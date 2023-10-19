9d45d56bf8c9f68a4e9fd5c594701f951e11dda2
Max Frank <fran1424@d.umn.edu>
2023-10-19 14:28:48 -0500

    added continuation token parsing and fetching all pages

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-import requests, json
+import requests, json, re
 from base64 import b64encode
 
 channelID = "UCTkXRDQl0luXxVQrRQvWS6w"
 zyphID = "UCPXGFu34px86DdXwocV-bYA"
 
+# Payload needed for request, contains arbitrary bytes of unknown origin
 firstPagePayload = b"\x12\x09community\xf2\x06\x04\x0a\x02\x4a\x00"
+# JSON data needed for request
 firstPageJSON = {
     "context": {"client": {"clientName": "WEB", "clientVersion": "2.20231016"}},
     "browseId": channelID, "params": b64encode(firstPagePayload).decode()
 }
+
+# Request first page of community posts
 response = requests.post(
     "https://www.youtube.com/youtubei/v1/browse?prettyprint=false",
     headers = {"Content-Type": "application/json"},
     data = json.dumps(firstPageJSON))
-responseJSON = response.json()
 
-print(json.dumps(responseJSON))
+while True:
+    # Get continuation payload from response
+    contMatch = re.findall('{"token": {0,1}".*?"}', response.text)
+    if len(contMatch) != 1:
+        break
+    contPayload = json.loads(contMatch[0])["token"]
+    print(contPayload)
+
+    # JSON data needed for request
+    contJSON = {
+        "context": {"client": {"clientName": "WEB", "clientVersion": "2.20231016"}},
+        "browseId": channelID, "continuation": contPayload
+    }
+    # Request next page of community posts
+    response = requests.post(
+        "https://www.youtube.com/youtubei/v1/browse?prettyprint=false",
+        headers = {"Content-Type": "application/json"},
+        data = json.dumps(contJSON))
