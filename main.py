9c58b7d24f850b615eaf8845e14a000ba8e208ed
Max Frank <fran1424@d.umn.edu>
2023-10-19 13:44:29 -0500

    clean up

--- main.py
+++ main.py
@@ -3,57 +3,17 @@ from base64 import b64encode
 
 channelID = "UCTkXRDQl0luXxVQrRQvWS6w"
 zyphID = "UCPXGFu34px86DdXwocV-bYA"
-hexPayload = "1209" + "community".encode("utf-8").hex() + "f206040a024a00"
 
-
-dataInitial = {"context": {"client": {"clientName": "WEB", "clientVersion": "2.20231016"}},
+firstPagePayload = "1209" + "community".encode("utf-8").hex() + "f206040a024a00"
+firstPageJSON = {
+    "context": {"client": {"clientName": "WEB", "clientVersion": "2.20231016"}},
     "browseId": channelID,
-    "params": b64encode(bytes.fromhex(hexPayload)).decode()
+    "params": b64encode(bytes.fromhex(firstPagePayload)).decode()
 }
 response = requests.post(
     "https://www.youtube.com/youtubei/v1/browse?prettyprint=false",
     headers = {"Content-Type": "application/json"},
-    data = json.dumps(dataInitial))
+    data = json.dumps(firstPageJSON))
 responseJSON = response.json()
 
 print(json.dumps(responseJSON))
-"""
-dataCont = {
-    "context": {"client": {"clientName": "WEB", "clientVersion": "2.20231016"}},
-    "browseId": channelID,
-    "continuation": b64encode(bytes.fromhex(cont1Payload)).decode()
-}
-response = requests.post(
-    "https://www.youtube.com/youtubei/v1/browse?prettyprint=false",
-    headers = {"Content-Type": "application/json"},
-    data = json.dumps(dataCont))
-"""
-
-"""
-cont1Payload = (
-    "e2a985b2028d011218" +
-    channelID.encode("utf-8").hex() +
-    "1a58" +
-    b64encode(bytes.fromhex(
-        "1209" +
-        "community".encode("utf-8").hex() +
-        "aa03280a245132684352465272546e646b4d477853556a425754564a496244566a4d565a4452554642280a" +
-        "f206040a024a00"
-    )).decode().replace("=", "%3D").encode("utf-8").hex() +
-    "9a0216" +
-    "backstage-item-section".encode("utf-8").hex()
-)
-cont2Payload = (
-    "e2a985b2028d011218" +
-    channelID.encode("utf-8").hex() +
-    "1a58" +
-    b64encode(bytes.fromhex(
-        "1209" +
-        "community".encode("utf-8").hex() +
-        "aa03280a245132684352465646556c4a694d317075556d7457536c4a47634735594d6a6c45525546422813" +
-        "f206040a024a00"
-    )).decode().replace("=", "%3D").encode("utf-8").hex() +
-    "9a0216" +
-    "backstage-item-section".encode("utf-8").hex()
-)
-"""
