30c3f9e44fcec08b40db56b6056671c979e67909
Max Frank <fran1424@d.umn.edu>
2023-10-19 15:27:25 -0500

    Cleaned up code

--- main.py
+++ main.py
@@ -4,35 +4,24 @@ from base64 import b64encode
 channelID = "UCTkXRDQl0luXxVQrRQvWS6w"
 zyphID = "UCPXGFu34px86DdXwocV-bYA"
 
-# Payload needed for request, contains arbitrary bytes of unknown origin
-firstPagePayload = b"\x12\x09community\xf2\x06\x04\x0a\x02\x4a\x00"
-# JSON data needed for request
-firstPageJSON = {
+# JSON data sent in POST to request community pages
+# params is a magic string that's used to get the community tab, no idea what it's for
+requestData = {
     "context": {"client": {"clientName": "WEB", "clientVersion": "2.20231016"}},
-    "browseId": channelID, "params": b64encode(firstPagePayload).decode()
+    "browseId": channelID, "params": "Egljb21tdW5pdHnyBgQKAkoA"
 }
 
-# Request first page of community posts
-response = requests.post(
-    "https://www.youtube.com/youtubei/v1/browse?prettyprint=false",
-    headers = {"Content-Type": "application/json"},
-    data = json.dumps(firstPageJSON))
+# Fetch community pages
+newPage = True
+while newPage:
+    # Request community page from the API
+    response = requests.post("https://www.youtube.com/youtubei/v1/browse?prettyprint=false",
+    headers = {"Content-Type": "application/json"}, data = json.dumps(requestData))
 
-while True:
-    # Get continuation payload from response
+    # Get continuation data from response (regex is simpler than trying to parse the JSON)
+    # Continuation data is a magic string we need to send the API to get the next page
     contMatch = re.findall('{"token": {0,1}".*?"}', response.text)
-    if len(contMatch) != 1:
-        break
-    contPayload = json.loads(contMatch[0])["token"]
-    print(contPayload)
-
-    # JSON data needed for request
-    contJSON = {
-        "context": {"client": {"clientName": "WEB", "clientVersion": "2.20231016"}},
-        "browseId": channelID, "continuation": contPayload
-    }
-    # Request next page of community posts
-    response = requests.post(
-        "https://www.youtube.com/youtubei/v1/browse?prettyprint=false",
-        headers = {"Content-Type": "application/json"},
-        data = json.dumps(contJSON))
+    if len(contMatch) == 1:
+        requestData["continuation"] = json.loads(contMatch[0])["token"]
+    else:
+        newPage = False
