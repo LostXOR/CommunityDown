b96f201c8653342214b897ff265826b877f38715
LostXOR <lostxor@gmail.com>
2023-10-27 13:35:15 -0500

    Made main actually work

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import communitydown, json
+from channel import Channel
 
 testChannel = "@CommunityDownTestChannel"
 
-channel = communitydown.Channel(testChannel)
+channel = Channel(testChannel)
 print(f"Channel exists: {channel.exists()}")
 print(f"Channnel ID: {channel.channelID()}")
 print(f"Channel has Community page: {channel.hasCommunity()}")
 
 posts = channel.fetchPosts()
 for post in posts:
-    print(communitydown.parsePost(post))
+    print(post.contentText)
+    comments = post.fetchComments(newestFirst = True)
+    print(comments)
