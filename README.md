b5037a2fe95efe1d13b26dbb1a25ac0ac86a911e
LostXOR <lostxor@gmail.com>
2023-10-31 14:41:23 -0500

    Added basic CLI and updated README

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
 # CommunityDown
 CommunityDown is a downloader for YouTube community pages. It is currently in early development and is only partially functional.
+## Usage
+```
+usage: main.py [-h] -c CHANNEL [-f {json}] -o OUTPUT [-P POST_LIMIT]
+                     [-C COMMENT_LIMIT] [-S {top,chronological}]
+
+A downloader for YouTube Community posts
+
+options:
+  -h, --help            show this help message and exit
+  -c CHANNEL, --channel CHANNEL
+                        The ID, username, or URL of the channel to export.
+  -f {json}, --format {json}
+                        The format of the export. Currently only JSON is
+                        supported.
+  -o OUTPUT, --output OUTPUT
+                        The file to export to.
+  -P POST_LIMIT, --post-limit POST_LIMIT
+                        The maximum number of posts to download. Defaults to
+                        all.
+  -C COMMENT_LIMIT, --comment-limit COMMENT_LIMIT
+                        The maximum number of comments to download. Defaults
+                        to all.
+  -S {top,chronological}, --comment-sort {top,chronological}
+                        Whether to use top or chronological comment sorting.
+```
 
 ## TODO
-Get comment replies
-
-Documentation
-
-A CLI
+- Refactor code into a Python module
+- Write better documentation
+- Add ability to fetch comment replies
+- Add HTML option for export format
+- Add more CLI options (exporting individual posts, comments, etc.)
--- main.py
+++ main.py
@@ -1,16 +1,62 @@
 """Just a test main for now, please ignore."""
 
 from channel import Channel
+import argparse
+import json
 
-TEST_CHANNEL = "@CommunityDownTestChannel"
+# Create parser for CLI arguments
+parser = argparse.ArgumentParser(
+    description = "A downloader for YouTube Community posts"
+)
+parser.add_argument("-c", "--channel", required = True,
+    help = "The ID, username, or URL of the channel to export.")
+parser.add_argument("-f", "--format", choices = ["json"], default = "json",
+    help = "The format of the export. Currently only JSON is supported.")
+parser.add_argument("-o", "--output", required = True,
+    help = "The file to export to.")
+parser.add_argument("-P", "--post-limit", default = -1, type = int,
+    help = "The maximum number of posts to download. Defaults to all.")
+parser.add_argument("-C", "--comment-limit", default = -1, type = int,
+    help = "The maximum number of comments to download. Defaults to all.")
+parser.add_argument("-S", "--comment-sort",
+    choices = ["top", "chronological"], default = "top",
+    help = "Whether to use top or chronological comment sorting.")
+args = parser.parse_args()
 
-channel = Channel(TEST_CHANNEL)
-print(f"Channel exists: {channel.exists()}")
-print(f"Channnel ID: {channel.channel_id()}")
-print(f"Channel has Community page: {channel.has_community()}")
+# Get channel
+print("Fetching channel...")
+channel = Channel(args.channel)
 
-posts = channel.fetch_posts()
-for post in posts:
-    print(post.contentText)
-    comments = post.fetch_comments(chronological = True)
-    print(comments)
+if not channel.exists():
+    print(f"Channel does not exist.")
+    exit()
+if not channel.has_community():
+    print(f"Channel does not have a Community page.")
+    exit()
+
+export = []
+
+# Fetch posts
+print("Fetching posts from channel...")
+posts = channel.fetch_posts(limit = args.post_limit)
+
+# Fetch comments
+if args.comment_limit != 0:
+    print(f"Fetching comments for {len(posts)} posts...")
+    print(f"0/{len(posts)}", end = "")
+    count = 0
+    for post in posts:
+        comments = post.fetch_comments(
+            limit = args.comment_limit,
+            chronological = args.comment_sort == "chronological")
+        export.append(post.data())
+        export[0]["comments"] = [c.data() for c in comments]
+        count += 1
+        print(f"\r{count}/{len(posts)}", end = "")
+    print()
+
+# Write export to file
+print("Saving to file...")
+with open(args.output, "w") as file:
+    file.write(json.dumps({"posts": export}))
+print(f"Export saved to {args.output}.")
